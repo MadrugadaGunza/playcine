5f5489bf55c5f96ffef13a9cd5ee26056709f7a8
MadrugadaGunza <madrugadaoca@gmail.com>
2026-04-04 07:09:14 +0100

    feat: codition search for popular movies from 10 to 5

--- src/appwrite.js
+++ src/appwrite.js
@@ -34,7 +34,7 @@ export const getTrendingMovies = async () => {
     try {
         const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
             Query.orderDesc("count"),
-            Query.limit(10)
+            Query.limit(5)
         ]);
         return result.documents;
     } catch (error) {
